0b7804d21b7d976b247732fd843f89bcff3857e7
Dani <danisuesmith@gmail.com>
2022-05-02 18:19:45 -0700

    commit tree d51a0b08d4b8cf1532fc56de1c6cec9c8529ba8a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+multiply;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+const num3 = 18;
+const num4 = 7;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(5, 10, 15, 20);
+max;
